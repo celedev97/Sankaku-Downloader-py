dc650a0495c9990bbee8736a08f4640d6b4c2139
fcdev <feddycap97@gmail.com>
2021-05-26 04:30:24 +0200

    updated user-agent

--- Settings.py
+++ Settings.py
@@ -1,5 +1,5 @@
 API_URL = "https://capi-v2.sankakucomplex.com/"
-HTTP_HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
+HTTP_HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'}
 
 POST_ID = "id"
 POST_URL = "file_url"
