bb7678fd8d540ea98cef3e48299c5ccb71220b1a
fcdev <feddycap97@gmail.com>
2021-05-26 04:35:45 +0200

    small refactoring
    renamed mainWindow to main so that pyinstaller doesn't make problems.
    removed the settings file, they weren't even settings, they were just random Sankaku constants in a file

--- Settings.py
+++
@@ -1,6 +0,0 @@
-API_URL = "https://capi-v2.sankakucomplex.com/"
-HTTP_HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'}
-
-POST_ID = "id"
-POST_URL = "file_url"
-POST_MIME = "file_type"
